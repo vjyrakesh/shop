170bffc760633e8a3edb7653b84d39dd55703411
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2025-11-09 13:04:22 +0000

    Add test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,13 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product one exists" do
+    product = products(:one)
+    assert product.name == "MyString"
+  end
+
+  test "product two exists" do
+    product = products(:two)
+    assert product.name == "MyString"
+  end
 end
